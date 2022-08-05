597c8dfb89a5e35854e27af2ae009e1867c1f873
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-06 00:20:09 +0530

    Implemented bulkhead FaultTolerance using Resilience4j.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -13,7 +13,7 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@RateLimiter(name = "default", fallbackMethod = "hardcodedResponse")
+	@Bulkhead(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample api call received");
 		return "sample api.";
