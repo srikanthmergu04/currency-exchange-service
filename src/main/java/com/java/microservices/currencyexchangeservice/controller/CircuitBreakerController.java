e2d33a167a34860ff2dd21d05c731283b6c5b826
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-05 23:19:10 +0530

    implemented API to fail.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -2,8 +2,10 @@ package com.java.microservices.currencyexchangeservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
@@ -13,7 +15,11 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	public String sampleApi() {
 		logger.info("Sample api call received");
-		return "sample-api";
+
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+				String.class);
+		return forEntity.getBody();
+
 	}
 
 	public String hardcodedResponse(Exception ex) {
