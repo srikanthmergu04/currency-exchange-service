655cb1f05c31bdc637f87cdd89d5179e668fb2f7
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-05 23:45:20 +0530

    Implemented Fault Tolerance using circuite breaker feature of Resilience4j.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,7 +15,7 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample api call received");
 
