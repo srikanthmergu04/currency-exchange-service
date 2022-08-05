97b94a7e9e0a8605c33f3bbd0347a16f729d5b6b
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-06 00:00:16 +0530

    Implemented Fault Tolerance using RateLimitar of Resilience4j.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.java.microservices.currencyexchangeservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,13 +13,10 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name = "default", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample api call received");
-
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-				String.class);
-		return forEntity.getBody();
+		return "sample api.";
 
 	}
 
