c8e4af3d53522477c58800e8617b3aff48cc25d5
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-05 15:11:53 +0530

    implemented JPA implementation.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -8,17 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.java.microservices.currencyexchangeservice.dao.CurrencyExchangeRepository;
 import com.java.microservices.currencyexchangeservice.model.CurrencyExchange;
 
 @RestController
 public class CurrencyExchangeController {
 
 	@Autowired
+	private CurrencyExchangeRepository currencyExchangeRepository;
+
+	@Autowired
 	private Environment environment;
 
 	@GetMapping("/currency-exchange/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+		if (currencyExchange == null) {
+			throw new RuntimeException("Unable to Find data for " + from + " to " + to);
+		}
 
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 
