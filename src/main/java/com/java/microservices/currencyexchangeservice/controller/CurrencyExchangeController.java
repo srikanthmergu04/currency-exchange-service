8c580e2ad6248d64bd784975b494c84d4333cbc3
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-08 17:33:57 +0530

    added Logger in CurrencyExchangeController.

--- src/main/java/com/java/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/java/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.java.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import com.java.microservices.currencyexchangeservice.model.CurrencyExchange;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -20,6 +24,9 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+		logger.info("getExchangeValue Called with {} to {}", from, to);
+
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 
 		if (currencyExchange == null) {
